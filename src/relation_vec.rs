32988e40ee0f0255e7d7e406fd4a38f95369417c
kampffrosch94 <kampffrosch94@github.com>
2024-12-28 23:54:34 +0100

    WIP almost got it

--- src/relation_vec.rs
+++ src/relation_vec.rs
@@ -1,6 +1,11 @@
 //! small vector optimization specifically for relations
 
-use std::{mem::ManuallyDrop, ops::Deref, ptr::NonNull};
+use std::{
+    alloc::{self, Layout},
+    mem::ManuallyDrop,
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+};
 
 const INLINE_COUNT: usize = 3;
 
@@ -8,10 +13,53 @@ struct RelationVecInline {
     elements: [u32; INLINE_COUNT],
 }
 
-#[repr(packed)]
+impl RelationVecInline {
+    pub fn new() -> Self {
+        RelationVecInline {
+            elements: [0; INLINE_COUNT],
+        }
+    }
+}
+
+#[repr(packed(4))]
 struct RelationVecOutline {
-    cap: u32,
     ptr: NonNull<u32>,
+    cap: u32,
+}
+
+impl RelationVecOutline {
+    fn new_alloc() -> RelationVecOutline {
+        let cap = 8;
+        let layout = Self::layout(cap);
+        let new_ptr = unsafe { alloc::alloc(layout) as *mut u32 };
+        let ptr = match NonNull::new(new_ptr) {
+            Some(p) => p,
+            None => alloc::handle_alloc_error(layout),
+        };
+        RelationVecOutline { ptr, cap }
+    }
+
+    fn layout(cap: u32) -> Layout {
+        Layout::from_size_align(size_of::<u32>() * cap as usize, align_of::<u32>()).unwrap()
+    }
+
+    unsafe fn write(&mut self, index: usize, val: u32) {
+        debug_assert!(index < self.cap as usize);
+        let dst = self.ptr.as_ptr().add(index);
+        unsafe { std::ptr::write(dst, val) };
+    }
+
+    unsafe fn grow(&mut self) {
+        let old_layout = RelationVecOutline::layout(self.cap);
+        self.cap *= 2;
+        let new_layout = RelationVecOutline::layout(self.cap);
+        let ptr = alloc::realloc(self.ptr.as_ptr() as *mut u8, old_layout, new_layout.size());
+        let ptr = match NonNull::new(ptr as *mut u32) {
+            Some(p) => p,
+            None => alloc::handle_alloc_error(new_layout),
+        };
+        self.ptr = ptr;
+    }
 }
 
 union RelationVecUnion {
@@ -27,9 +75,7 @@ struct RelationVec {
 impl RelationVec {
     pub fn new() -> Self {
         let content = RelationVecUnion {
-            inline: ManuallyDrop::new(RelationVecInline {
-                elements: [0; INLINE_COUNT],
-            }),
+            inline: ManuallyDrop::new(RelationVecInline::new()),
         };
         RelationVec { len: 0, content }
     }
@@ -38,28 +84,64 @@ impl RelationVec {
         self.len
     }
 
-    pub fn push(&mut self, new: u32) {
+    pub fn push(&mut self, new_val: u32) {
         // maybe sort? for binary search
         if self.len < INLINE_COUNT as u32 {
             let inline = unsafe { &mut self.content.inline };
-            inline.elements[self.len as usize] = new;
+            inline.elements[self.len as usize] = new_val;
         } else {
-            todo!();
+            if self.len == INLINE_COUNT as u32 {
+                // transform to outline vec
+                let inline = unsafe { &mut self.content.inline };
+                let mut outline = RelationVecOutline::new_alloc();
+                for i in 0..self.len as usize {
+                    unsafe { outline.write(i, inline.elements[i]) };
+                }
+                unsafe { ManuallyDrop::drop(inline) };
+                self.content = RelationVecUnion {
+                    outline: ManuallyDrop::new(outline),
+                };
+            }
+            unsafe {
+                let outline = &mut self.content.outline;
+                // grow if necessary
+                if self.len == outline.cap {
+                    outline.grow();
+                }
+                outline.write(self.len as usize, new_val);
+            }
         }
         self.len += 1;
     }
 
     pub fn remove(&mut self, element: u32) {
-        if self.len <= INLINE_COUNT as u32 {
-            let slice = &mut unsafe { &mut self.content.inline }.elements;
-            let index = slice.iter().position(|it| *it == element);
-            if let Some(index) = index {
+        let index = self[..].iter().position(|it| *it == element);
+        if let Some(index) = index {
+            if self.len <= INLINE_COUNT as u32 {
+                let slice = &mut unsafe { &mut self.content.inline }.elements;
+                let last = self.len - 1;
+                slice.swap(index, last as usize);
+            } else {
+                let slice = &mut self[..];
                 let last = slice.len() - 1;
                 slice.swap(index, last);
-                self.len -= 1;
+                if self.len == INLINE_COUNT as u32 + 1 {
+                    // convert back to inline
+                    let mut inline = RelationVecInline::new();
+                    for i in 0..INLINE_COUNT {
+                        inline.elements[i] = self[i];
+                    }
+                    let outline = unsafe { &mut self.content.outline };
+                    unsafe {
+                        outline.ptr.drop_in_place();
+                        ManuallyDrop::drop(outline);
+                    }
+                    self.content = RelationVecUnion {
+                        inline: ManuallyDrop::new(inline),
+                    };
+                }
             }
-        } else {
-            todo!();
+            self.len -= 1;
         }
     }
 }
@@ -95,6 +177,18 @@ impl Deref for RelationVec {
     }
 }
 
+impl DerefMut for RelationVec {
+    fn deref_mut(&mut self) -> &mut [u32] {
+        if self.len <= INLINE_COUNT as u32 {
+            let inline = unsafe { &mut self.content.inline };
+            return &mut inline.elements[0..self.len as usize];
+        } else {
+            let outline = unsafe { &self.content.outline };
+            unsafe { std::slice::from_raw_parts_mut(outline.ptr.as_ptr(), self.len as usize) }
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -106,7 +200,6 @@ mod test {
         assert_eq!(12, size_of::<RelationVecUnion>());
         assert_eq!(16, size_of::<RelationVec>());
     }
-
 
     #[test]
     fn inline_vec() {
@@ -129,4 +222,19 @@ mod test {
         assert_eq!(0, vec.len());
     }
 
+    #[test]
+    fn outline_vec() {
+        let mut vec = RelationVec::new();
+        assert_eq!(0, vec.len());
+        vec.push(10);
+        vec.push(20);
+        vec.push(30);
+        vec.push(40);
+        vec.push(50);
+        assert_eq!(5, vec.len());
+        assert_eq!(&[10, 20, 30, 40, 50], &vec[..]);
+        vec.remove(20);
+        vec.remove(30);
+        assert_eq!(&[10, 50, 40], &vec[..]);
+    }
 }
