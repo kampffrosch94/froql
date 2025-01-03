d6c6ba98d613ef156eaeed4013e4de8088a005e0
kampffrosch94 <kampffrosch94@github.com>
2025-01-03 09:41:55 +0100

    WIP query helper

--- src/query_helper.rs
+++ src/query_helper.rs
@@ -1,5 +1,105 @@
-use crate::world::World;
+use std::{any::TypeId, mem::MaybeUninit};
+
+use crate::{entity_store::{Entity, EntityId}, world::World};
 
 pub struct QueryHelper<'a> {
     world: &'a World,
+}
+
+/// RelationType, from, to
+type Relation = (TypeId, usize, usize);
+/// ComponentType, source
+type Component = (TypeId, usize);
+
+// OPTIMIZATION: keeep the body of this function as small as possible to save on monomorphisation
+/// used by the proc macro
+#[allow(unused)]
+pub unsafe fn relation_join_iter_components<'a, const VARS: usize, const COMPS: usize>(
+    world: &'a World,
+    relations: &[Relation],
+    components: [Component; COMPS],
+    unequals: &[(usize, usize)],
+    uncomponents: &[Component],
+    unrelations: &[Relation], 
+    prefill: &[(usize, Entity)],
+) -> impl Iterator<Item = ([Entity; VARS], [*const u8; COMPS])> + use<'a, VARS, COMPS> {
+    let join_table: JoinTable<'_, VARS> = JoinTable::new_init(
+        world,
+        relations,
+        &components,
+        unequals,
+        uncomponents,
+        unrelations,
+        prefill,
+    );
+    join_table.rows.into_iter().filter_map(move |row| {
+        let mut result = [MaybeUninit::<*const u8>::uninit(); COMPS];
+        for i in 0..COMPS {
+            let (tid, id) = components[i];
+            let res = unsafe { Some(todo!()) }?;
+            result[i].write(res);
+        }
+        Some((row, result.map(|entry| unsafe { entry.assume_init() })))
+    })
+}
+
+/// Temporary structure which helps with non trivial multi relationship joins
+pub struct JoinTable<'a, const N: usize> {
+    pub world: &'a World,
+    pub filled: [bool; N],
+    pub rows: Vec<[Entity; N]>,
+}
+
+impl<'a, const COLUMN_COUNT: usize> JoinTable<'a, { COLUMN_COUNT }> {
+    pub fn new(world: &'a World) -> Self {
+        JoinTable { world, filled: [false; COLUMN_COUNT], rows: Vec::new() }
+    }
+
+    pub fn new_init(
+        world: &'a World,
+        relations: &[Relation],
+        components: &[Component],
+        _unequals: &[(usize, usize)],
+        _uncomponents: &[Component], // Not needed anymore
+        _unrelations: &[Relation],
+        prefill: &[(usize, Entity)],
+    ) -> Self {
+        let mut join_table = JoinTable::new(world);
+        // init
+        match (components.is_empty(), prefill.is_empty(), relations.is_empty()) {
+            (true, true, true) => {}
+            (false, true, true) => {
+                //join_table.init_from_component(components[0]);
+                //join_table.join_components(&components[1..]);
+                if COLUMN_COUNT > 1 {
+                    unimplemented!("Cross Joins are not supported.")
+                }
+                todo!();
+            }
+            (_, true, false) => {
+                //let joins = compute_join_order(&relations);
+                //join_table.init_from_relation(joins[0]);
+                //join_table.join_relations(&joins[1..]);
+                todo!();
+            }
+            (_, false, true) => {
+                //join_table.prefill_columns(prefill);
+                //join_table.join_components(&components);
+                todo!();
+            }
+            (_, false, false) => {
+                //let joins = compute_join_order(&relations);
+                //join_table.prefill_columns(prefill);
+                //join_table.join_relations(&joins);
+                todo!();
+            }
+        }
+        //join_table.join_uncomponents(&uncomponents);
+        //join_table.join_unrelations(&unrelations);
+        //join_table.remove_by_constraint_unequals(unequals);
+        debug_assert!(join_table.filled.iter().all(|filled| *filled));
+        join_table
+    }
+
+
 }
