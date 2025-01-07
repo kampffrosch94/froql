638e671dff39940b089ae4b98dab60bb0558a971
kampffrosch94 <kampffrosch94@github.com>
2025-01-07 22:02:37 +0100

    WIP good idea

--- src/query_helper.rs
+++ src/query_helper.rs
@@ -2,7 +2,8 @@ use std::{any::TypeId, ptr::NonNull};
 
 use crate::{
     archetype::{ArchetypeId, ArchetypeRow},
-    entity_store::{Entity, EntityGeneration, EntityId},
+    bookkeeping::Bookkeeping,
+    entity_store::Entity,
     world::World,
 };
 
@@ -57,43 +58,175 @@ pub struct JoinTable<'a, const VAR_COUNT: usize, const COMP_COUNT: usize> {
     pub rows: Vec<([Entity; VAR_COUNT], [*const u8; COMP_COUNT])>,
 }
 
-pub struct QueryWorker<'a> {
-    pub world: &'a World,
-    pub result_entities: &'a mut [(ArchetypeId, ArchetypeRow)],
+pub struct ResultData<'a> {
+    pub var_archetype_id: &'a mut [ArchetypeId],
+    pub var_archetype_row: &'a mut [ArchetypeRow],
     pub result_components: &'a mut [NonNull<u8>],
-    pub ops: &'a mut [Op<'a>],
+    pub col_ids: &'a mut [usize],
 }
 
-impl<'a> QueryWorker<'a> {
-    // returns true if it produced a new result
-    // returns false if execution can not continue
-    pub fn process(&mut self) -> bool {
-        false
+pub fn grab_archetype_id(
+    var_archetype_id: &mut [ArchetypeId],
+    var_archetype_row: &mut [ArchetypeRow],
+    variable: usize,
+    archetype_set: &Vec<ArchetypeId>,
+    next_index: &mut usize, // needs to start with 0 before first call
+) -> bool {
+    if *next_index >= archetype_set.len() {
+        return false;
+    }
+    var_archetype_id[variable] = archetype_set[*next_index];
+    var_archetype_row[variable] = ArchetypeRow(u32::MAX);
+    *next_index += 1;
+    return true;
+}
+
+fn grab_archetype_row(
+    var_archetype_id: &mut [ArchetypeId],
+    var_archetype_row: &mut [ArchetypeRow],
+    variable: usize,
+    bk: &Bookkeeping,
+) -> bool {
+    let var_row = &mut var_archetype_row[variable];
+    let var_a = &var_archetype_id[variable];
+    if var_row.0 == u32::MAX {
+        var_row.0 = 0;
+    } else {
+        var_row.0 += 1;
+    }
+    if var_row.0 as usize >= bk.archetypes[var_a.0 as usize].entities.len() {
+        return false;
+    }
+    return true;
+}
+
+/// Iterator that continues as long as the inner closure returns Some()
+struct ClosureIterator<F> {
+    closure: F,
+}
+
+impl<F, T> Iterator for ClosureIterator<F>
+where
+    F: FnMut() -> Option<T>,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.closure)()
     }
 }
 
-pub enum Op<'a> {
-    GrabArchetype(&'a mut u32),
-    GrabRow(&'a mut u32),
+impl<F, T> ClosureIterator<F>
+where
+    F: FnMut() -> Option<T>,
+{
+    pub fn new(closure: F) -> Self {
+        Self { closure }
+    }
 }
 
-fn foobar() {
-    let world = World::new();
+#[cfg(test)]
+mod test {
+    use std::cell::RefCell;
 
-    let mut result_entities = [(ArchetypeId(u32::MAX), ArchetypeRow(u32::MAX)); 2];
-    let mut result_component = [NonNull::dangling(); 4];
-    let mut state = 32;
-    let mut ops = [Op::GrabRow(&mut state)];
-    let mut worker = QueryWorker {
-        world: &world,
-        result_entities: &mut result_entities,
-        result_components: &mut result_component,
-        ops: &mut ops,
+    use crate::{
+        archetype::{ArchetypeId, ArchetypeRow},
+        query_helper::{grab_archetype_id, grab_archetype_row, ClosureIterator},
+        world::World,
     };
-    while worker.process() {
-        dbg!(&worker.result_entities[0]);
-    }
-}
 
-fn grab_archetype() {
+    #[test]
+    fn manual_query_helper_trivial() {
+        #[derive(Debug)]
+        struct CompA(usize);
+        #[derive(Debug)]
+        struct CompB(String);
+        struct CompC {}
+
+        let mut world = World::new();
+        let a = world.create();
+        world.add_component(a, CompA(42));
+        world.add_component(a, CompB("Hello".to_string()));
+        let b = world.create();
+        world.add_component(b, CompA(21));
+        let c = world.create();
+        world.add_component(c, CompA(42));
+        world.add_component(c, CompB("Hello".to_string()));
+        world.add_component(c, CompC {});
+
+        let mut counter = 0;
+        for (comp_a, comp_b) in {
+            let world: &World = &world;
+            let bk = &world.bookkeeping;
+            let components = [
+                world.get_component_id::<CompA>(),
+                world.get_component_id::<CompB>(),
+            ];
+            let archetype_ids = bk.matching_archetypes(&components, &[]);
+
+            // array length is amount of variable
+            const VAR_COUNT: usize = 1;
+            let mut a_ids = [ArchetypeId(u32::MAX); VAR_COUNT];
+            let mut a_rows = [ArchetypeRow(u32::MAX); VAR_COUNT];
+            let mut col_ids = [usize::MAX; 2];
+
+            //let mut result_component = [NonNull::dangling(); 4];
+
+            let mut next_index = 0;
+
+            let mut current_step = 0;
+            ClosureIterator::new(move || {
+                loop {
+                    match current_step {
+                        // next archetype
+                        0 => {
+                            if grab_archetype_id(
+                                &mut a_ids,
+                                &mut a_rows,
+                                0,
+                                &archetype_ids,
+                                &mut next_index,
+                            ) {
+                                let arch = &bk.archetypes[a_ids[0].0 as usize];
+                                arch.find_multiple_columns(&components, &mut col_ids[0..2]);
+                                current_step += 1;
+                            } else {
+                                return None;
+                            }
+                        }
+                        // next row in archetype
+                        1 => {
+                            // TODO just define maxlength as context and iterate to that
+                            let next = grab_archetype_row(&mut a_ids, &mut a_rows, 0, bk);
+                            current_step += if next { 1 } else { -1 };
+                        }
+                        // yield row
+                        2 => {
+                            let arch = &bk.archetypes[a_ids[0].0 as usize];
+                            let row = a_rows[0].0;
+                            current_step -= 1;
+                            return Some(unsafe {
+                                (
+                                    (&*((&arch.columns[col_ids[0]]).get(row)
+                                        as *const RefCell<CompA>))
+                                        .borrow(),
+                                    (&*((&arch.columns[col_ids[1]]).get(row)
+                                        as *const RefCell<CompB>))
+                                        .borrow(),
+                                )
+                            });
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+            })
+        } {
+            println!("{comp_a:?}");
+            println!("{comp_b:?}");
+            assert_eq!(42, comp_a.0);
+            assert_eq!("Hello", &comp_b.0);
+            counter += 1;
+        }
+        assert_eq!(2, counter);
+    }
 }
