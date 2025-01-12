0f44c7b6cdde3af1ccb06f9f4f41a6e91555caae
kampffrosch94 <kampffrosch94@github.com>
2025-01-12 10:59:15 +0100

    compute join order

--- derive/src/generator.rs
+++ derive/src/generator.rs
@@ -3,10 +3,12 @@ use std::{collections::HashMap, ops::Range};
 
 use crate::{Component, Relation};
 
-#[derive(Debug)]
-struct VarInfo {
+#[derive(Debug, Clone)]
+pub struct VarInfo {
+    /// Index of this variable
+    index: isize,
     /// other var index, index for relation component
-    related_with: HashMap<isize, usize>,
+    related_with: HashMap<(String, isize), usize>,
     /// indexes in component array
     component_range: Range<usize>,
 }
@@ -25,6 +27,7 @@ fn generate_archetype_sets(
 
     for var in vars {
         let mut info = VarInfo {
+            index: *var,
             related_with: HashMap::new(),
             component_range: index..index,
         };
@@ -39,7 +42,7 @@ fn generate_archetype_sets(
             result.push_str(&format!(
                 "\n    bk.get_component_id_unchecked(TypeId::of::<Relation<{ty}>>()),"
             ));
-            info.related_with.insert(*other, index);
+            info.related_with.insert((ty.clone(), *other), index);
             index += 1;
             info.component_range.end += 1;
         }
@@ -49,11 +52,12 @@ fn generate_archetype_sets(
             result.push_str(&format!(
                 "bk.get_component_id_unchecked(TypeId::of::<Relation<{ty}>>()).flip_target(),"
             ));
-            info.related_with.insert(*other, index);
+            info.related_with.insert((ty.clone(), *other), index);
             index += 1;
             info.component_range.end += 1;
         }
         result.push_str("\n];\n\n");
+        infos.push(info);
     }
 
     result.push_str("let archetype_id_sets = [\n");
@@ -90,9 +94,10 @@ let mut col_indexes = [usize::MAX; {col_count}];
     ));
 }
 
-fn generate_resumeable_query_closure(
+fn generate_resumable_query_closure(
     result: &mut String,
     vars: &[isize],
+    infos: &[VarInfo],
     components: &[Component],
     relations: &[Relation],
 ) {
@@ -108,26 +113,68 @@ std::iter::fstd::iter::from_fn(move || { loop { match current_step {
     );
 }
 
-pub fn compute_join_order(relations: &[Relation]) -> Vec<Relation> {
-    let mut joined: Vec<Relation> = Vec::new();
-    let mut avail: Vec<Relation> = Vec::from(relations);
-    joined.push(avail.pop().unwrap());
-    for _ in 0..avail.len() {
-        let new = avail.iter().position(|(_, c, d)| {
-            joined
-                .iter()
-                .any(|(_, a, b)| b == c || a == c || a == d || b == d)
-        });
-        if let Some(pos) = new {
-            // surely there is a better way to go about this
-            // prototype code atm
-            let rel = avail.remove(pos);
-            joined.push(rel);
+#[derive(Debug)]
+enum JoinKind {
+    /// component id, old var, new var
+    NewJoin(usize, isize, isize),
+    /// component id, old var, new var
+    RelationConstraint(usize, isize, isize),
+}
+
+pub fn compute_join_order(relations: &[Relation], infos: &[VarInfo]) -> (isize, Vec<JoinKind>) {
+    // TODO figure out the variable to start with
+    // I think its a decent metric to use the most constrained variable first
+    let first = infos
+        .iter()
+        .max_by_key(|it| it.component_range.len())
+        .unwrap();
+
+    let mut result: Vec<JoinKind> = Vec::new();
+    let mut available: Vec<VarInfo> = vec![first.clone()];
+    let mut work_left: Vec<Relation> = Vec::from(relations);
+    // TODO compute join
+    for _ in 0..work_left.len() {
+        // find next viable for joining and remove it from working list
+        let next_constraint = {
+            let pos = work_left.iter().position(|rel| {
+                available
+                    .iter()
+                    .any(|avail| avail.index == rel.1 && avail.index == rel.2)
+            });
+            pos.map(|pos| work_left.remove(pos))
+        };
+        if let Some(constraint) = next_constraint {
+            let old = constraint.1;
+            let new = constraint.2;
+            let info = &infos[old as usize];
+            assert_eq!(old, info.index);
+            let comp_index = info.related_with[&(constraint.0, constraint.2)];
+            result.push(JoinKind::RelationConstraint(comp_index, old, new));
         } else {
-            panic!("Cross joins are not supported. Use nested queries instead.");
+            let next_join = {
+                let pos = work_left.iter().position(|rel| {
+                    available
+                        .iter()
+                        .any(|avail| avail.index == rel.1 || avail.index == rel.2)
+                });
+                pos.map(|pos| work_left.remove(pos))
+            };
+            if let Some(join) = next_join {
+                let reversed = available.iter().any(|it| it.index == join.2);
+                let old = if reversed {join.2} else {join.1};
+                let new = if reversed {join.1} else {join.2};
+                let info = &infos[old as usize];
+                assert_eq!(old, info.index);
+                dbg!(info);
+                let comp_index = info.related_with[&(join.0, new)];
+                result.push(JoinKind::NewJoin(comp_index, old, new));
+                available.push(infos[new as usize].clone());
+            } else {
+                panic!("Cross joins are not supported.")
+            }
         }
     }
-    return joined;
+    return (first.index, result);
 }
 
 #[cfg(test)]
@@ -135,13 +182,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_generate_archteype_id_sets_relation() {
+    fn test_generate_archetype_id_sets_relation() {
         let components = vec![("Unit".into(), 0), ("Health".into(), 0), ("Unit".into(), 1)];
         let relations = vec![("Attack".into(), 1, 0)];
         let vars = vec![0, 1];
         let mut result = String::new();
+        let infos;
         insta::assert_snapshot!({
-            generate_archetype_sets(&mut result, &vars, &components, &relations);
+            infos = generate_archetype_sets(&mut result, &vars, &components, &relations);
             result
         }, @r#"
         let components_0 = [
@@ -160,10 +208,49 @@ mod test {
             bk.matching_archetypes(&components_1, &[]),
         ];
         "#);
+
+        insta::assert_debug_snapshot!(infos, @r#"
+        [
+            VarInfo {
+                index: 0,
+                related_with: {
+                    (
+                        "Attack",
+                        1,
+                    ): 2,
+                },
+                component_range: 0..3,
+            },
+            VarInfo {
+                index: 1,
+                related_with: {
+                    (
+                        "Attack",
+                        0,
+                    ): 4,
+                },
+                component_range: 3..5,
+            },
+        ]
+        "#);
+
+        let join_order = compute_join_order(&relations, &infos);
+        insta::assert_debug_snapshot!(join_order, @r#"
+        (
+            0,
+            [
+                NewJoin(
+                    2,
+                    0,
+                    1,
+                ),
+            ],
+        )
+        "#);
     }
 
     #[test]
-    fn test_generate_archteype_id_sets_trivial() {
+    fn test_generate_archetype_id_sets_trivial() {
         let components = vec![("Pos".into(), 0), ("Speed".into(), 0)];
         let relations = [];
         let vars = vec![0];
@@ -189,6 +276,7 @@ mod test {
         let relations = vec![("Attack".into(), 1, 0)];
         let vars = vec![0, 1];
         let mut result = String::new();
+
         insta::assert_snapshot!({
             generate_fsm_context(&mut result, &vars, &components, &relations);
             result
@@ -207,13 +295,15 @@ mod test {
     }
 
     #[test]
-    fn test_generate_resumeable_query_closure() {
+    fn test_generate_resumable_query_closure() {
         let components = vec![("Unit".into(), 0), ("Health".into(), 0), ("Unit".into(), 1)];
         let relations = vec![("Attack".into(), 1, 0)];
         let vars = vec![0, 1];
         let mut result = String::new();
+        let infos = generate_archetype_sets(&mut String::new(), &vars, &components, &relations);
         insta::assert_snapshot!({
-            generate_resumeable_query_closure(&mut result, &vars, &components, &relations);
+            generate_resumable_query_closure(&mut result, &vars,
+                                              &infos,&components, &relations);
             result
         }, @r#"
         // context for this specific statemachine
@@ -227,16 +317,31 @@ mod test {
 
     #[test]
     fn test_compute_join_order() {
-        let relations = vec![("Attack".into(), 1, 0), ("Bla".into(), 0, 1)];
-        let joined = compute_join_order(&relations);
-        let s = joined
-            .iter()
-            .map(|(ty, from, to)| format!("({ty}, {from}, {to})"))
-            .collect::<Vec<_>>()
-            .join("\n");
-        insta::assert_snapshot!(s, @r#"
-        (Bla, 0, 1)
-        (Attack, 1, 0)
+        let relations = vec![("Attack".into(), 1, 0)];
+        let infos = vec![
+            VarInfo {
+                index: 0,
+                related_with: HashMap::from([(("Attack".into(), 1), 2)]),
+                component_range: 0..3,
+            },
+            VarInfo {
+                index: 1,
+                related_with: HashMap::from([(("Attack".into(), 0), 4)]),
+                component_range: 4..5,
+            },
+        ];
+        let joined = compute_join_order(&relations, &infos);
+        insta::assert_debug_snapshot!(joined, @r#"
+        (
+            0,
+            [
+                NewJoin(
+                    2,
+                    0,
+                    1,
+                ),
+            ],
+        )
         "#);
     }
 }
