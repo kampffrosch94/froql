8621f38090f3cce411e836920027cc2bf378f20e
kampffrosch94 <kampffrosch94@github.com>
2025-02-07 18:04:11 +0100

    put join order into own module

--- froql_macro/src/generator.rs
+++ froql_macro/src/generator.rs
@@ -3,15 +3,19 @@ use std::fmt::Debug;
 use std::fmt::Write;
 use std::{collections::HashMap, ops::Range};
 
+use join_order::compute_join_order;
+use join_order::JoinOrder;
+use join_order::NewJoin;
+
 use crate::generator_nodes::archetype_start::ArchetypeStart;
 use crate::generator_nodes::invar_start::InvarInfo;
 use crate::generator_nodes::invar_start::InvarStart;
 use crate::generator_nodes::relation_helper::RelationHelperInfo;
 use crate::generator_nodes::relation_join::RelationJoin;
-use crate::generator_nodes::types::RelationConstraint;
 use crate::generator_nodes::GeneratorNode;
 use crate::ANYVAR;
 use crate::{Accessor, Component, Relation};
+mod join_order;
 
 #[derive(Default, Debug)]
 pub struct Generator {
@@ -329,8 +333,12 @@ pub(crate) fn generate_resumable_query_closure(
     assert_eq!(infos.len(), vars.len());
     let prepend = result;
     let mut append = String::new();
-    let (first, invar_unequals, invar_rel_constraints, join_order) =
-        compute_join_order(relations, infos, prefills, unequals);
+    let JoinOrder {
+        first,
+        invar_unequals,
+        invar_rel_constraints,
+        join_order,
+    } = compute_join_order(relations, infos, prefills, unequals);
 
     assert!(
         infos.iter().all(|it| it.init_rank.is_some()),
@@ -395,6 +403,7 @@ pub(crate) fn generate_resumable_query_closure(
         .generate(step_count, prepend, &mut append);
     }
 
+    // TODO put yield into generator node
     // yield row
     write!(
         &mut append,
@@ -473,182 +482,10 @@ _ => unreachable!(),
     prepend.push_str(&append);
 }
 
-#[derive(Debug)]
-struct NewJoin {
-    new: isize,
-    unequal_constraints: Vec<(isize, isize)>,
-    rel_constraints: Vec<RelationConstraint>,
-}
-
-fn compute_join_order(
-    relations: &[Relation],
-    infos: &mut [VarInfo],
-    prefills: &HashMap<isize, String>,
-    unequals: &[(isize, isize)],
-) -> (
-    isize,
-    Vec<(isize, isize)>,
-    Vec<RelationConstraint>,
-    Vec<NewJoin>,
-) {
-    let mut result = Vec::new();
-    let mut available: Vec<isize> = Vec::new();
-    let mut unequals = Vec::from(unequals);
-    let mut work_left: Vec<Relation> = relations
-        .iter()
-        .cloned()
-        // anyvars only matter as components for constraining archetype sets
-        .filter(|(_, from, to)| *from != ANYVAR && *to != ANYVAR)
-        .collect();
-
-    // figure out what to start with
-    let mut init_rank = 0;
-    if prefills.is_empty() {
-        // I think its a decent metric to use the most constrained variable first
-        let first = infos
-            .iter_mut()
-            .max_by_key(|it| it.component_range.len())
-            .unwrap();
-        first.init_rank = Some(init_rank);
-        init_rank += 1;
-        available.push(first.index);
-    } else {
-        for (var, _) in prefills {
-            available.push(*var);
-        }
-        available.sort();
-        for var in &available {
-            infos[*var as usize].init_rank = Some(init_rank);
-            init_rank += 1;
-        }
-    }
-
-    let mut relation_helper_nr = 0;
-
-    // using a closure so I don't have to duplicate this part
-    let mut newly_available_unequals = |available: &mut Vec<isize>| {
-        let mut result = Vec::new();
-        while let Some(index) = unequals
-            .iter()
-            .position(|(a, b)| available.contains(a) && available.contains(b))
-        {
-            let (a, b) = unequals[index];
-            result.push((a, b));
-            unequals.swap_remove(index);
-        }
-        result
-    };
-    let newly_available_constraints =
-        |available: &mut Vec<isize>,
-         work_left: &mut Vec<Relation>,
-         infos: &mut [VarInfo],
-         relation_helper_nr: &mut usize| {
-            let mut result = Vec::new();
-            while let Some(index) = work_left
-                .iter()
-                .position(|(_, a, b)| available.contains(a) && available.contains(b))
-            {
-                let (comp_name, a, b) = work_left[index].clone();
-                let (old, new) = if infos[a as usize].init_rank.unwrap()
-                    < infos[b as usize].init_rank.unwrap()
-                {
-                    (a, b)
-                } else {
-                    (b, a)
-                };
-                let old_info = &mut infos[old as usize];
-                assert_eq!(old, old_info.index);
-                let column_index = old_info.related_with[&(comp_name, new)];
-                result.push(RelationConstraint {
-                    helper_nr: *relation_helper_nr,
-                    checked_invar: Some(new),
-                });
-                work_left.swap_remove(index);
-
-                let cid_index = column_index - old_info.component_range.start;
-                old_info.relation_helpers.push(RelationHelperInfo {
-                    column_index,
-                    old_var: old,
-                    new_var: new,
-                    nr: *relation_helper_nr,
-                    cid_index,
-                });
-                let new_info = &mut infos[new as usize];
-                new_info.join_helper_index = Some(*relation_helper_nr);
-                *relation_helper_nr += 1;
-            }
-            result
-        };
-
-    let invar_unequals = newly_available_unequals(&mut available);
-    let invar_constraints = newly_available_constraints(
-        &mut available,
-        &mut work_left,
-        infos,
-        &mut relation_helper_nr,
-    );
-
-    // compute join
-    while !work_left.is_empty() {
-        // find next viable for joining and remove it from working list
-        // always handle constraints first, because it may let us skip work
-        let next_join = {
-            let pos = work_left.iter().position(|rel| {
-                available
-                    .iter()
-                    .any(|avail| *avail == rel.1 || *avail == rel.2)
-            });
-            pos.map(|pos| work_left.remove(pos))
-        };
-        if let Some(join) = next_join {
-            let reversed = available.iter().any(|avail| *avail == join.2);
-            let old_var = if reversed { join.2 } else { join.1 };
-            let new_var = if reversed { join.1 } else { join.2 };
-            let old_info = &mut infos[old_var as usize];
-            assert_eq!(old_var, old_info.index);
-            let column_index = old_info.related_with[&(join.0, new_var)];
-            let cid_index = column_index - old_info.component_range.start;
-
-            old_info.relation_helpers.push(RelationHelperInfo {
-                column_index,
-                old_var,
-                new_var,
-                nr: relation_helper_nr,
-                cid_index,
-            });
-            let new_info = &mut infos[new_var as usize];
-            new_info.join_helper_index = Some(relation_helper_nr);
-            new_info.init_rank = Some(init_rank);
-            init_rank += 1;
-            relation_helper_nr += 1;
-
-            available.push(new_var);
-            let unequal_constraints = newly_available_unequals(&mut available);
-            let mut relation_constraints = newly_available_constraints(
-                &mut available,
-                &mut work_left,
-                infos,
-                &mut relation_helper_nr,
-            );
-            for rc in &mut relation_constraints {
-                rc.checked_invar = None; // there must be a better design than this, lol
-            }
-            result.push(NewJoin {
-                new: new_var,
-                unequal_constraints,
-                rel_constraints: relation_constraints,
-            });
-        } else {
-            panic!("Cross joins are not supported. Use nested queries instead.")
-        }
-    }
-    assert!(unequals.is_empty());
-
-    return (available[0], invar_unequals, invar_constraints, result);
-}
-
 #[cfg(test)]
 mod test {
+    use join_order::compute_join_order;
+
     use super::*;
     use crate::Accessor;
 
@@ -737,28 +574,28 @@ mod test {
 
         let join_order = compute_join_order(&relations, &mut infos, &prefills, &[]);
         insta::assert_debug_snapshot!(join_order, @r#"
-        (
-            0,
-            [],
-            [],
-            [
+        JoinOrder {
+            first: 0,
+            invar_unequals: [],
+            invar_rel_constraints: [],
+            join_order: [
                 NewJoin {
                     new: 1,
                     unequal_constraints: [],
                     rel_constraints: [],
                 },
             ],
-        )
+        }
         "#);
 
         let unequals = vec![(0, 1)];
         let join_order = compute_join_order(&relations, &mut infos, &prefills, &unequals);
         insta::assert_debug_snapshot!(join_order, @r#"
-        (
-            0,
-            [],
-            [],
-            [
+        JoinOrder {
+            first: 0,
+            invar_unequals: [],
+            invar_rel_constraints: [],
+            join_order: [
                 NewJoin {
                     new: 1,
                     unequal_constraints: [
@@ -770,7 +607,7 @@ mod test {
                     rel_constraints: [],
                 },
             ],
-        )
+        }
         "#);
     }
 
---
+++ froql_macro/src/generator/join_order.rs
@@ -0,0 +1,191 @@
+use std::collections::HashMap;
+
+use crate::{
+    generator_nodes::{relation_helper::RelationHelperInfo, types::RelationConstraint},
+    Relation, ANYVAR,
+};
+
+use super::VarInfo;
+
+#[derive(Debug)]
+pub struct NewJoin {
+    pub new: isize,
+    pub unequal_constraints: Vec<(isize, isize)>,
+    pub rel_constraints: Vec<RelationConstraint>,
+}
+
+#[derive(Debug)]
+pub struct JoinOrder {
+    pub first: isize,
+    pub invar_unequals: Vec<(isize, isize)>,
+    pub invar_rel_constraints: Vec<RelationConstraint>,
+    pub join_order: Vec<NewJoin>,
+}
+
+pub fn compute_join_order(
+    relations: &[Relation],
+    infos: &mut [VarInfo],
+    prefills: &HashMap<isize, String>,
+    unequals: &[(isize, isize)],
+) -> JoinOrder {
+    let mut result = Vec::new();
+    let mut available: Vec<isize> = Vec::new();
+    let mut unequals = Vec::from(unequals);
+    let mut work_left: Vec<Relation> = relations
+        .iter()
+        .cloned()
+        // anyvars only matter as components for constraining archetype sets
+        .filter(|(_, from, to)| *from != ANYVAR && *to != ANYVAR)
+        .collect();
+
+    // figure out what to start with
+    let mut init_rank = 0;
+    if prefills.is_empty() {
+        // I think its a decent metric to use the most constrained variable first
+        let first = infos
+            .iter_mut()
+            .max_by_key(|it| it.component_range.len())
+            .unwrap();
+        first.init_rank = Some(init_rank);
+        init_rank += 1;
+        available.push(first.index);
+    } else {
+        // if we have prefills we just start with those
+        for (var, _) in prefills {
+            available.push(*var);
+        }
+        available.sort();
+        for var in &available {
+            infos[*var as usize].init_rank = Some(init_rank);
+            init_rank += 1;
+        }
+    }
+
+    let mut relation_helper_nr = 0;
+
+    // using a closure so I don't have to duplicate this part
+    let mut newly_available_unequals = |available: &mut Vec<isize>| {
+        let mut result = Vec::new();
+        while let Some(index) = unequals
+            .iter()
+            .position(|(a, b)| available.contains(a) && available.contains(b))
+        {
+            let (a, b) = unequals[index];
+            result.push((a, b));
+            unequals.swap_remove(index);
+        }
+        result
+    };
+    let newly_available_constraints =
+        |available: &mut Vec<isize>,
+         work_left: &mut Vec<Relation>,
+         infos: &mut [VarInfo],
+         relation_helper_nr: &mut usize| {
+            let mut result = Vec::new();
+            while let Some(index) = work_left
+                .iter()
+                .position(|(_, a, b)| available.contains(a) && available.contains(b))
+            {
+                let (comp_name, a, b) = work_left[index].clone();
+                let (old, new) = if infos[a as usize].init_rank.unwrap()
+                    < infos[b as usize].init_rank.unwrap()
+                {
+                    (a, b)
+                } else {
+                    (b, a)
+                };
+                let old_info = &mut infos[old as usize];
+                assert_eq!(old, old_info.index);
+                let column_index = old_info.related_with[&(comp_name, new)];
+                result.push(RelationConstraint {
+                    helper_nr: *relation_helper_nr,
+                    checked_invar: Some(new),
+                });
+                work_left.swap_remove(index);
+
+                let cid_index = column_index - old_info.component_range.start;
+                old_info.relation_helpers.push(RelationHelperInfo {
+                    column_index,
+                    old_var: old,
+                    new_var: new,
+                    nr: *relation_helper_nr,
+                    cid_index,
+                });
+                let new_info = &mut infos[new as usize];
+                new_info.join_helper_index = Some(*relation_helper_nr);
+                *relation_helper_nr += 1;
+            }
+            result
+        };
+
+    let invar_unequals = newly_available_unequals(&mut available);
+    let invar_rel_constraints = newly_available_constraints(
+        &mut available,
+        &mut work_left,
+        infos,
+        &mut relation_helper_nr,
+    );
+
+    // compute join
+    while !work_left.is_empty() {
+        // find next viable for joining and remove it from working list
+        // always handle constraints first, because it may let us skip work
+        let next_join = {
+            let pos = work_left.iter().position(|rel| {
+                available
+                    .iter()
+                    .any(|avail| *avail == rel.1 || *avail == rel.2)
+            });
+            pos.map(|pos| work_left.remove(pos))
+        };
+        if let Some(join) = next_join {
+            let reversed = available.iter().any(|avail| *avail == join.2);
+            let old_var = if reversed { join.2 } else { join.1 };
+            let new_var = if reversed { join.1 } else { join.2 };
+            let old_info = &mut infos[old_var as usize];
+            assert_eq!(old_var, old_info.index);
+            let column_index = old_info.related_with[&(join.0, new_var)];
+            let cid_index = column_index - old_info.component_range.start;
+
+            old_info.relation_helpers.push(RelationHelperInfo {
+                column_index,
+                old_var,
+                new_var,
+                nr: relation_helper_nr,
+                cid_index,
+            });
+            let new_info = &mut infos[new_var as usize];
+            new_info.join_helper_index = Some(relation_helper_nr);
+            new_info.init_rank = Some(init_rank);
+            init_rank += 1;
+            relation_helper_nr += 1;
+
+            available.push(new_var);
+            let unequal_constraints = newly_available_unequals(&mut available);
+            let mut relation_constraints = newly_available_constraints(
+                &mut available,
+                &mut work_left,
+                infos,
+                &mut relation_helper_nr,
+            );
+            for rc in &mut relation_constraints {
+                rc.checked_invar = None; // there must be a better design than this, lol
+            }
+            result.push(NewJoin {
+                new: new_var,
+                unequal_constraints,
+                rel_constraints: relation_constraints,
+            });
+        } else {
+            panic!("Cross joins are not supported. Use nested queries instead.")
+        }
+    }
+    assert!(unequals.is_empty());
+
+    return JoinOrder {
+        first: available[0],
+        invar_unequals,
+        invar_rel_constraints,
+        join_order: result,
+    };
+}
